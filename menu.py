27000d19ba239306457377d72274a187388c12f6
Maskyss <irisiri99@rambler.ru>
2019-05-04 19:55:55 +0300

    add menu

---
+++ menu.py
@@ -0,0 +1,14 @@
+def menu1():
+    print("Running Server(Debug mode)?                         Press 1")
+    print("Without Server?                                     Press 2")
+    return input()
+
+
+def menu2():
+    print("Print all current tasks?                         Press 1")
+    print("Add new task?                                    Press 2")
+    print("Delete task with id?                             Press 3")
+    print("Update task with id?                             Press 4")
+    print("Exit from the program:                           Press 0")
+
+    return input()
