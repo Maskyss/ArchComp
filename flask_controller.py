dee8193eb83123d0cf28302716d0d7c9b4419c3f
levYKpi <43970766+levYKpi@users.noreply.github.com>
2019-05-05 15:41:29 +0300

    Update flask_controller.py

--- flask_controller.py
+++ flask_controller.py
@@ -15,3 +15,34 @@ tasks = [
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'ServerPart': 'Yasenko'},
+                                 {'UiPart': 'Sirenko'},
+                                 {'TestPart': 'Antoniuk'}), 404)
+
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': tasks})
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'done': request.json['done'],
+        'day': request.json['day'],
+        'month': request.json['month'],
+        'year': request.json['year']
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
